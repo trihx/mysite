a0910dcad33a834b2c929929dc642b6ea0bb55c2
Hoang Xuan Tri <xuantri.hoang@gmail.com>
2019-05-09 16:05:40 +0700

    part 3

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,30 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
+from django.shortcuts import render
+from django.template import loader
+from .models import Question
 
 
 def index(request):
-    return HttpResponse("Hello world, you're at the index page")
+    # latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    # output = ', '.join([q.question_text for q in latest_question_list])
+    # return HttpResponse(output)
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
+
+
+def detail(request, question_id):
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExits:
+        raise Http404("Question does not exit")
+    return render(request, 'pools/detail.html', {'question': question})
+
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
